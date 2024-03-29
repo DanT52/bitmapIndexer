68361bf4ab40c30af590ae4ea0bc2cf44184103c
DanT52 <dantramp52@gmail.com>
2024-03-29 10:01:00 -0700

    work on bbc

--- bbccomp.py
+++ bbccomp.py
@@ -1,36 +1,38 @@
 def compress_bbc_line(dataline):
     compressed_line = []
     
-    current_run_length = 0
+    num_of_runs = 0
     current_literals = []
+    num_of_lits = 0
+    isDirtyBit = False
+    dirty_bit_position = None
 
     def flush_compression():
-        nonlocal compressed_line, current_run_length, current_literals
-        header_byte = 0
+        nonlocal compressed_line, num_of_runs, current_literals, num_of_lits, isDirtyBit, dirty_bit_position
+        ending_byte = ""
+        header = ""
 
-        if current_run_length > 0:
-            if current_run_length <= 6:
-                # Encode run length in the header if <= 6
-                header_byte = current_run_length
-            elif 7 <= current_run_length <= 127:
-                # For run lengths 7-127, set header to 111 and add a byte
-                header_byte = 0b11100000
-                compressed_line.append(header_byte)
-                compressed_line.append(current_run_length)
-            else:
-                # For run lengths 128-32767, use two bytes
-                header_byte = 0b11100000
-                compressed_line.append(header_byte)
-                compressed_line += [0b10000000 | (current_run_length >> 8), current_run_length & 0xFF]
-            current_run_length = 0
-        if current_literals:
-            # Adjust the header for literals if not just flushing runs
-            literals_len = len(current_literals)
-            if literals_len <= 15:
-                header_byte |= (literals_len << 4)
-                compressed_line.insert(-len(current_literals)-1, header_byte)
-            compressed_line += current_literals
-            current_literals = []
+        if num_of_runs <7:
+            pass
+        elif num_of_runs >= 7 and num_of_runs < 127:
+            pass
+        elif num_of_runs >= 127 and num_of_runs < 32768:
+            pass
+
+        if isDirtyBit:
+            header+= "1"
+            header += bin(dirty_bit_position)[2:].zfill(4)
+            compressed_line += header + ending_byte
+            flush_compression()
+            return
+
+        else:
+            header+= "0"
+            header += bin(num_of_lits)[2:].zfill(4)
+
+        
+
+        
 
     for index in range(0, len(dataline), 8):
         segment = dataline[index:index+8]
