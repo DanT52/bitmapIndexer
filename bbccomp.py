9a7e1da54cd293a7cf9dffcf6f77ddf13b68be47
Daniel Basarab <dantramp52@gmail.com>
2024-03-31 22:14:46 -0700

    test script

--- bbccomp.py
+++ bbccomp.py
@@ -12,7 +12,9 @@ def compress_bbc_line(dataline):
         ending_byte = ""
         header = ""
 
-        if num_of_runs <7:
+        if num_of_runs == 0:
+            header+= "000"
+        elif num_of_runs <7:
             pass
         elif num_of_runs >= 7 and num_of_runs < 127:
             pass
@@ -60,7 +62,46 @@ def compress_bbc_line(dataline):
 dataline_432_zeros_3_literals = ['0'] * 432 + ['10101010', '10101010', '10101010']
 expected_output_432_zeros_3_literals = "11100011 10000001 10110000 10101010 10101010 10101010"
 
-compressed_line_432_zeros_3_literals = compress_bbc_line(dataline_432_zeros_3_literals)
+# compressed_line_432_zeros_3_literals = compress_bbc_line(dataline_432_zeros_3_literals)
 
-print("Compressed Line:", compressed_line_432_zeros_3_literals)
-print("Matches Expected:", compressed_line_432_zeros_3_literals == expected_output_432_zeros_3_literals)
+# print("Compressed Line:", compressed_line_432_zeros_3_literals)
+# print("Matches Expected:", compressed_line_432_zeros_3_literals == expected_output_432_zeros_3_literals)
+
+def print_file_8_chars_at_a_time(file_path, occurrence):
+    with open(file_path, 'r') as file:
+        line_count = 0
+        skip_count = 0
+        header = False
+        count_after_0001 = 0
+        stop_after = 5
+        found_0001 = 0
+        while True:
+            header = False
+
+            chars = file.read(8)
+            if not chars or (found_0001 == occurrence and count_after_0001 >= stop_after):
+                break
+
+            if skip_count == 0:
+                print("# ", end='')
+                header = True
+            if skip_count == 0 and chars[3] == '0':
+                skip_count = int(chars[4:], 2)
+                print(skip_count)
+
+            if chars.startswith('0001') and header:
+                print(chars + ' ****')
+                found_0001 += 1
+            elif skip_count > 0 and not header:
+                print(chars + ' !')
+                skip_count -= 1
+            else:
+                print(chars)
+
+            if found_0001 == occurrence:
+                count_after_0001 += 1
+
+            line_count += 1
+
+print_file_8_chars_at_a_time("mine/compressed/animals_BBC_8", 6)
+
